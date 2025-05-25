c34e7291ebfe61050d3b0e37cafa8152dc2ab129
merkoffski <merkoffski@me.com>
2025-05-25 13:53:39 +0100

    Add files via upload

---
+++ api/airtable/tables.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  const token = "patBKToDXZ9amoYZu.9f2cd5d6f123218ccc801b9572f13c843d789a016fda5004c9348d4d7c4b91e2";
+  const baseId = req.query.baseId;
+
+  if (!baseId) {
+    return res.status(400).json({ error: "Missing baseId" });
+  }
+
+  const response = await fetch(`https://api.airtable.com/v0/meta/bases/${baseId}/tables`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json"
+    }
+  });
+
+  const data = await response.json();
+  res.status(200).json(data.tables);
+}
